59c68eff8e7121a9c803bfb761732074dc8931be
Sanchi Vaishnavi <vaishnavi.sanchi@gmail.com>
2025-06-08 12:13:31 +0000

    Let CXX compiler be user defined

--- src/bench-acpp.hpp
+++ src/bench-acpp.hpp
@@ -1,22 +1,34 @@
 /****************************
  * Benchmarking AdaptiveCpp *
 *****************************/
+#include "CustomIterator.hpp"
 
 template <typename T>
 std::pair<double, double> bench_acpp_min_element(int N, int NTIMES) {
-
-  int counter = 0;  
-  auto gen = [N, &counter](int &i){
-    i = (counter > 10 ?  counter : N-counter);
-    counter++;
-    };
+  auto pol = std::execution::par_unseq;
 
   T* data = new T[N];
-  std::for_each(data, data+N, gen);
+  auto gen = [data, N](T idx) {
+    data[idx] = (idx > 10) ? idx : N - idx;
+  };
 
-  auto pol = std::execution::par_unseq;
+  std::vector<std::size_t> indices(N);
+  std::iota(indices.begin(), indices.end(), 0);
+  std::for_each(pol, indices.begin(), indices.end(), gen);
+
+  // Aksel:
+  // A better, but more complex solution is to use
+  // std::ranges::iota_view, or a custom iterator class
+  // that just stores the current index and implements
+  // dereferencing by just returning the index.
+  // Then you can just use CustomIterator<size_t>{0, N}.begin()
+  // and CustomIterator<size_t>{0, N}.end() in your for_each.
+  // The lambda will then get the index as argument.
+
+  // std::for_each(pol, CustomIterator<T>{0, N}.begin(),
+  //               CustomIterator<T>{0, N}.end(), gen);
+
   auto comp = std::less<>{};
-
   // cache warm-up
   auto res = std::min_element(pol, data, data + N);
 
@@ -28,10 +40,10 @@ std::pair<double, double> bench_acpp_min_element(int N, int NTIMES) {
     time_vector.push_back((end - start) * 1e-9);
   }
 
-  auto duration = std::min_element(time_vector.begin(), time_vector.end());
+  double duration = * std::min_element(time_vector.begin(), time_vector.end());
   double GBytes = (double (N) * sizeof(int)) * 1.0e-9;
-  double bandwidth = ( GBytes ) / *duration; // Note the changed bw since we measure
-                                             // the minimum duration instead of avg.
+  double bandwidth = ( GBytes ) / duration; // Note the changed bw since we measure
+                                            // the minimum duration instead of avg.
 
   std::cout << "# AdaptiveCpp: std::min_element";
   std::cout << "\n# Verification: " << *res << ", at:  " << std::distance(data, res);
--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,5 +1,11 @@
-// acpp-23.10.0/move-build/install/bin/acpp --acpp-targets="generic" -ltbb -O3 -march=native --acpp-stdpar godbolt.cpp
-// OMP_NUM_THREADS=32 ACPP_ADAPTIVITY_LEVEL=2 ./a.out -- ACPP_VISIBILITY_MASK=omp
+// acpp-23.10.0/move-build/install/bin/acpp --acpp-targets="generic" -ltbb -O3
+// -march=native --acpp-stdpar benchmark.cpp -o bench.acpp
+// ACPP_VISIBILITY_MASK=omp ACPP_ADAPTIVITY_LEVEL=2 ./bench.acpp
+
+// cmake -DCMAKE_CXX_COMPILER=clang++-18
+//       -DKokkos_ROOT=<path-to-kokkos>/builddir/install
+//       -DKokkos_DIR=<path-to-kokkos>/builddir/install/lib/cmake/Kokkos
+//       ..
 
 #include <iostream>
 #include <algorithm>
@@ -20,22 +26,21 @@
 int main() {
   // Attempt to fill up L3 cache. Xeon R Gold has L3 = 22MB => 44 MB for two node setup
   // Vector size should be 3.8 times the cache = 83,6MB and total memory req = ?
-  int N = 20e7; // 10e6 elements to fill one node. 
+  int N = 10e8; // 10e6 elements to fill one node.
   int NTIMES = 100; // TODO
 
   #if defined (USEACPP)
   auto pmin = bench_acpp_min_element<int>(N, NTIMES);
-
-  std::cout << "\n# memory (MB)" << " bandwidth (GB/s)\n  "
-                << pmin.first * 1000 << "      " << pmin.second << std::endl;
   #endif
 
   #if defined (USEKOKKOS)
-  auto pmin = bench_kokkos_min_element<int>(N, NTIMES);
-
-  std::cout << "std::min_element\n# memory (MB)" << " bandwidth (GB/s)\n  "
-                << pmin.first * 1000 << "      " << pmin.second << std::endl;
+  Kokkos::initialize();
+    auto pmin = bench_kokkos_min_element<Kokkos::Cuda, int>(N, NTIMES);
+  Kokkos::finalize();
   #endif
+
+  std::cout << "\n# memory (MB)" << " bandwidth (GB/s)\n  "
+                << pmin.first * 1000 << "      " << pmin.second << std::endl;
 
   return 0;
 }
